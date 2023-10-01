19248195a01e9037e71dfdecf091cbc40c40b2ce
Shah Jalal <shahjalalislam001166@gmail.com>
2023-10-01 11:09:00 +0600

    Front page design done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
-    
+    // Hero Slick
+    $('.innovate-hero__slick-parent').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        infinite: true,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }
+          ]
+    });
 });
-
